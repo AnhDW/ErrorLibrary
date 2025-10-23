21d45dc6cfe6b76097a0d3bbff13158f984c6939
TanHuynhNLU <hntan2000@gmail.com>
2025-10-23 09:43:05 +0700

    feat: build solution library UI

---
+++ ErrorLibrary/Controllers/SolutionLibraryController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ErrorLibrary.Controllers
+{
+    public class SolutionLibraryController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
