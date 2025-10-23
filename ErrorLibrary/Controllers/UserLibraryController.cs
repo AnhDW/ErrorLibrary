4ccf024e776e4aa4595edec5be7086edead37aed
TanHuynhNLU <hntan2000@gmail.com>
2025-10-23 10:24:07 +0700

    feat: build user library UI

---
+++ ErrorLibrary/Controllers/UserLibraryController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ErrorLibrary.Controllers
+{
+    public class UserLibraryController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
