09bda0eb3384a4cb29fc1989ef4bf513f16f7f6a
vu anh <vungocanh723@gmail.com>
2025-10-24 16:56:47 +0700

    update

---
+++ ErrorLibrary/wwwroot/js/page-load.js
@@ -0,0 +1,9 @@
+﻿$(document).ready(function () {
+    const path = window.location.pathname;
+
+    if (path.includes('/page-a')) {
+        loadDataForPageA();
+    } else if (path.includes('/page-b')) {
+        loadDataForPageB();
+    }
+});
