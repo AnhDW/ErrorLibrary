b6ef2e30a475143a2310541dd008a7f789026bfd
TanHuynhNLU <hntan2000@gmail.com>
2025-12-03 15:35:55 +0700

    feat: basic layout InLine

---
+++ ErrorLibrary/Controllers/InLineController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ErrorLibrary.Controllers
+{
+    public class InLineController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
