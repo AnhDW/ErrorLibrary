ccc7be4f91ca46d9758219d125b65100e63b8495
Anh Vũ <vungocanh300999@gmail.com>
2025-12-03 21:56:37 +0700

    update

---
+++ ErrorLibrary/wwwroot/js/page-handle/in-line-library.js
@@ -0,0 +1,45 @@
+﻿var treeData = [
+    {
+        text: "Parent 1",
+        nodes: [
+            {
+                text: "Child 1",
+                nodes: [
+                    {
+                        text: "Grandchild 1"
+                    },
+                    {
+                        text: "Grandchild 2"
+                    }
+                ]
+            },
+            {
+                text: "Child 2"
+            }
+        ]
+    },
+    {
+        text: "Parent 2"
+    },
+    {
+        text: "Parent 3"
+    },
+    {
+        text: "Parent 4"
+    },
+    {
+        text: "Parent 5"
+    }
+];
+
+$('#tree').treeview({
+    data: treeData,
+    onNodeSelected: function (event, node) {
+        // Gán giá trị node vào hidden input
+        $('#selectedNode').val(node.id);
+        // Hiển thị tên node trên nút dropdown
+        $('#treeDropdown').text(node.text);
+        // Đóng dropdown
+        $('.dropdown-menu').removeClass('show');
+    }
+});
