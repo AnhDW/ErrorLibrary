6aa9e1e4919f806f22db0d33d005d72a0f8fb628
TanHuynhNLU <hntan2000@gmail.com>
2025-12-12 09:46:17 +0700

    endline ui

---
+++ ErrorLibrary/Controllers/EndLineLibraryController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ErrorLibrary.Controllers
+{
+    public class EndLineLibraryController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
