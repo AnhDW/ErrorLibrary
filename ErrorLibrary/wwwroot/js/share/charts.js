7e01c4c81104b0d9c49b8543c47baf639142da0f
TanHuynhNLU <hntan2000@gmail.com>
2025-12-16 15:22:42 +0700

    feat: build ui Report page

---
+++ ErrorLibrary/wwwroot/js/share/charts.js
@@ -0,0 +1,26 @@
+﻿function renderTopErrorChart() {
+    var options = {
+        series: [25, 30, 45],
+        chart: {
+            width: 380,
+            type: 'pie',
+        },
+        labels: ['Lỗi 1', 'Lỗi 2', 'Lỗi 3'],
+        responsive: [{
+            breakpoint: 480,
+            options: {
+                chart: {
+                    width: 200
+                },
+                legend: {
+                    position: 'bottom'
+                }
+            }
+        }]
+    };
+
+    var chart = new ApexCharts(document.querySelector("#topErrorChart"), options);
+    chart.render();
+}
+
+renderTopErrorChart();
