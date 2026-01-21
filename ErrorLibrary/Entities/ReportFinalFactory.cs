460cae4575a8785488d528be587a0b8116bb5e91
vu anh <vungocanh723@gmail.com>
2026-01-21 17:09:54 +0700

    update

---
+++ ErrorLibrary/Entities/ReportFinalFactory.cs
@@ -0,0 +1,10 @@
+﻿namespace ErrorLibrary.Entities
+{
+    public class ReportFinalFactory
+    {
+        public int Id { get; set; }
+        public int FactoryId { get; set; }
+        public string Name { get; set; }
+        public DateTime CreateDate { get; set; }
+    }
+}
